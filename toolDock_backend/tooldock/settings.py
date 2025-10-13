ab0932ef2078361433209e2246896018923cf45a
eyobScript <eyobbmulugeta@gmail.com>
2025-10-14 01:41:33 +0300

    Create template for home page

--- toolDock_backend/tooldock/settings.py
+++ toolDock_backend/tooldock/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'tooldock.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [ BASE_DIR / 'playground/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
