486c705885fb8e91ce602d3a861b10ff73621a72
eyobScript <eyobbmulugeta@gmail.com>
2025-10-14 23:29:57 +0300

    updated

--- toolDock_backend/entrypoint.sh
+++ toolDock_backend/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 set -e
 
-echo "Running migrations..."
+echo "Waiting for MySQL to be ready..."
+
+until nc -z -v -w30 $DB_HOST $DB_PORT
+do
+  echo "Waiting for database connection at $DB_HOST:$DB_PORT..."
+  sleep 5
+done
+
+echo "MySQL is up - running migrations..."
 python manage.py migrate --noinput
 
-echo "Starting Django development server..."
+echo "Starting Django server..."
 python manage.py runserver 0.0.0.0:8000
