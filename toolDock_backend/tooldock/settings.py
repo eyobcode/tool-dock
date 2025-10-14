73597f95eb042ab80383468f71698006af6956ae
eyobScript <eyobbmulugeta@gmail.com>
2025-10-14 04:37:59 +0300

    install celery and redis for broker

--- toolDock_backend/tooldock/settings.py
+++ toolDock_backend/tooldock/settings.py
@@ -127,3 +127,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Redis
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
