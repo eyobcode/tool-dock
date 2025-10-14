16d4b927ddc09f78c9f7a1c04c036d08d4975627
eyobScript <eyobbmulugeta@gmail.com>
2025-10-14 17:16:03 +0300

    Input type model

--- toolDock_backend/scans/models.py
+++ toolDock_backend/scans/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from uuid import uuid4
 
-# Create your models here.
+
+
+class InputType(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
