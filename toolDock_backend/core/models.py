004596b52647c354ebf62015fe7f19446bec2a22
eyobScript <eyobbmulugeta@gmail.com>
2025-10-19 01:28:04 +0300

    add user independent class

--- toolDock_backend/core/models.py
+++ toolDock_backend/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
 
+
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ("user", "User"),
+        ("admin", "Admin"),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="user")
