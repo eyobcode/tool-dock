62cbfb9302cfc9d176bef0fef658d50621b25797
eyobScript <eyobbmulugeta@gmail.com>
2025-10-20 16:54:56 +0300

    update

--- toolDock_backend/entrypoint.sh
+++ toolDock_backend/entrypoint.sh
@@ -12,5 +12,6 @@ done
 echo "MySQL is up - running migrations..."
 python manage.py migrate --noinput
 
-echo "Starting Django server..."
-python manage.py runserver 0.0.0.0:8000
+# ---- Run the command passed by docker-compose ----
+echo "Starting service: $@"
+exec "$@"
