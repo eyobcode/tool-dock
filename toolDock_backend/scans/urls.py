cae19e818950ae776e12733702ca54db5f5c40a0
eyobScript <eyobbmulugeta@gmail.com>
2025-10-23 23:55:03 +0300

    upadted

--- toolDock_backend/scans/urls.py
+++ toolDock_backend/scans/urls.py
@@ -3,5 +3,6 @@ from .views import ScanViewSet
 
 router = DefaultRouter()
 router.register(r'start', ScanViewSet, basename='scan')
+# router.register(r'results/<uuid:pk>/', ResultView, basename='result )
 
 urlpatterns = router.urls
