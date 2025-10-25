53168a5937c4a3e04f79a583d83d9d625886db8a
eyobScript <eyobbmulugeta@gmail.com>
2025-10-26 01:21:15 +0300

    updated viewset

--- toolDock_backend/scans/views.py
+++ toolDock_backend/scans/views.py
@@ -1,26 +1,46 @@
-from datetime import timezone as dt_timezone
-from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
-from rest_framework import status
-from rest_framework.response import Response
-from .models import ScanJob
-from .serializers import ScanSerializer,GetScanSerializer
-from .tasks import run_scan_task
-import uuid
-from .utils import get_tool_runner
 import json
-import time
-from collections import Counter
 
-from collections import defaultdict
-from rest_framework import status
-from rest_framework.response import Response
+
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-import uuid
-import json
-import time
-from datetime import timezone as dt_timezone
+from rest_framework import status
+
 from .models import ScanJob
-from .serializers import ScanSerializer, GetScanSerializer
+from .serializers import ScanSerializer,ScanResultSerializer
 from .tasks import run_scan_task
+
+
+
+    
+
+class ScanViewSet(CreateModelMixin, GenericViewSet):
+    queryset = ScanJob.objects.select_related('findings').all()
+    serializer_class = ScanSerializer
+
+    def _parsed_options(self):
+        raw_options = self.request.data.get("options") # type: ignore
+        if raw_options in (None, "", "null"):
+            return {}
+        try:
+            return json.loads(raw_options)
+        except json.JSONDecodeError:
+            return {}
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        job = serializer.save()
+
+
+        scan_type = self._parsed_options().get("scan_type")
+
+
+        if scan_type == "quick":
+            result_serializer = ScanResultSerializer(job)
+            run_scan_task.apply(args=[str(job.job_id)]).get()
+            return Response({"ok": True, "data": result_serializer.data}, status=status.HTTP_200_OK)
+        
+        run_scan_task.apply(args=[str(job.job_id)])
+        response_data = ScanSerializer(job).data
+        return Response({"ok": True, "data": response_data}, status=status.HTTP_202_ACCEPTED)
