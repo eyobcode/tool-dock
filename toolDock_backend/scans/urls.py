4f7a8bb5076a946beb648b216377bf640a18553c
eyobScript <eyobbmulugeta@gmail.com>
2025-11-05 15:47:34 +0300

    created the retrieves url

--- toolDock_backend/scans/urls.py
+++ toolDock_backend/scans/urls.py
@@ -1,8 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import ScanViewSet
+from .views import ScanViewSet,ScanResultViewSet
 
 router = DefaultRouter()
 router.register(r'start', ScanViewSet, basename='scan')
-# router.register(r'results/<uuid:pk>/', ResultView, basename='result )
+router.register(r'results', ScanResultViewSet, basename='result')
 
 urlpatterns = router.urls
