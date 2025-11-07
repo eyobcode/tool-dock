79f8713dfff9365a23d6c31073874a503ed89ee7
eyobScript <eyobbmulugeta@gmail.com>
2025-11-07 04:29:08 +0300

    serializers with authentication

--- toolDock_backend/scans/urls.py
+++ toolDock_backend/scans/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import ScanViewSet,ScanResultViewSet
+from .views import ScanViewSet,ScanResultViewSet, ScanHistoryViewSet
 
 router = DefaultRouter()
 router.register(r'start', ScanViewSet, basename='scan')
 router.register(r'results', ScanResultViewSet, basename='result')
+router.register(r'histories', ScanHistoryViewSet, basename='history')
 
 urlpatterns = router.urls
