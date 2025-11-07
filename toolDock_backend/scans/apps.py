40a21ce5b612321e774837465bd75f9272f64598
eyobScript <eyobbmulugeta@gmail.com>
2025-11-07 20:58:33 +0300

    create signal to create profile for new user

--- toolDock_backend/scans/apps.py
+++ toolDock_backend/scans/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ScansConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scans'
+
+    def ready(self)-> None:
+        import scans.signales.handlers
+
