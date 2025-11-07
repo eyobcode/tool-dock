f3354b9653739e13cb9c926c1ff32851b08839df
eyobScript <eyobbmulugeta@gmail.com>
2025-11-07 04:29:10 +0300

    serializers with authentication

--- toolDock_backend/tooldock/urls.py
+++ toolDock_backend/tooldock/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('playground.urls')),
     path('scans/', include('scans.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
